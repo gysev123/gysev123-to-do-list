534657c6246ea6246d417d61ec28e483a21e5091
gysev123 <gysev702@gmail.com>
2025-10-05 00:06:41 +0500

    to-do create
    локальное хранилище

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ const List = document.getElementById("list");
 const newList = document.createElement("li");
 const button = document.getElementById("agree");
 const editbtn = document.getElementById("edit");
-const notes = [];
+const data = localStorage.getItem("note");
+const notes = data ? JSON.parse(data) : [];
 
 button.onclick = function () {
   addNote();
 };
+
+function clear() {
+  localStorage.clear();
+}
 
 function addNote() {
   const textPole = document.getElementById("text").value;
@@ -18,12 +23,14 @@ function addNote() {
     title: textPole,
     completed: false,
   });
+
   document.getElementById("text").value = "";
   editbtn.innerHTML = "";
   render();
 }
 
 function render() {
+  localStorage.setItem("note", JSON.stringify(notes));
   List.innerHTML = "";
   if (notes.length == 0) {
     List.innerHTML = `<p class="warning">пусто</p>`;
@@ -35,8 +42,9 @@ function render() {
 
 function createList(notes, index) {
   let spanClass = notes.completed ? "valueListC" : "valueList";
+  let item = notes.completed ? "itemtrue" : "itemfalse";
   return `
-          <li>
+          <li class="${item}">
             <span class="${spanClass}">${notes.title}</span>
               <span class="buttonList">
                 <span class="editList" data-index="${index}" data-type="edit">1</span>
@@ -54,9 +62,12 @@ List.onclick = function (event) {
     if (type == "toggle") {
       notes[index].completed = !notes[index].completed;
     } else if (type == "remove") {
+      button.style.display = "block";
       editbtn.innerHTML = "";
       notes.splice(index, 1);
+      document.getElementById("text").value = "";
     } else if (type == "edit") {
+      button.style.display = "none";
       editNote(index);
       document.getElementById("text").value = notes[index].title;
     }
@@ -66,7 +77,8 @@ List.onclick = function (event) {
 };
 
 function editNote(index) {
-  editbtn.innerHTML = `<button data-index="${index}" data-type="editBtn" class="btnEdit">отредактировать</button>`;
+  editbtn.innerHTML = `<button data-index="${index}" data-type="editBtn" class="btnEdit">отредактировать</button>
+  <button data-index="${index}" data-type="editBtnClose" class="btnEditClose">Х</button>`;
 }
 
 editbtn.onclick = function (event) {
@@ -78,7 +90,12 @@ editbtn.onclick = function (event) {
     if (type == "editBtn") {
       notes[index].title = textPole;
       editbtn.innerHTML = "";
+      button.style.display = "block";
       document.getElementById("text").value = "";
+    } else if (type == "editBtnClose") {
+      editbtn.innerHTML = "";
+      document.getElementById("text").value = "";
+      button.style.display = "block";
     }
   }
   render();
